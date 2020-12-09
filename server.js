218e341951bc60b946bd907cfc154540cb982e16
Cassandra Kornhiser <cvkornhiser@gmail.com>
2020-12-09 15:48:30 -0800

    Error handling

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ const server = app.listen(port, () => {
 
 process.on('unhandledRejection', (err) => {
   console.log('UNHANDLED REJECTION! 💥 Shutting down...');
-  console.log(err.name, err.message);
+  console.log(err.name, err.message, err.err);
   //gracefully closes down the server
   server.close(() => {
     process.exit(1);
