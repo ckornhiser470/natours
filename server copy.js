b8b0db86780d81cbb116fefec2691c0292f3b410
Cassandra Kornhiser <cvkornhiser@gmail.com>
2020-12-09 19:27:13 -0800

    Spelled config wrong

--- server copy.js
+++ server copy.js
@@ -4,26 +4,38 @@ const dotenv = require('dotenv');
 process.on('uncaughtException', (err) => {
   console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
   console.log(err.name, err.message, err.stack);
-  console.log(process.env.DATABASE);
   process.exit(1);
 });
-
-dotenv.config({ path: './config.env' });
+//WANT THIS UNCAUGHT EXCEPTION AT THE TOP TO LISTEN THRU TO CHECK
 const app = require('./app');
 
+dotenv.config({
+  path: './confing.env',
+});
+
 const DB = process.env.DATABASE.replace(
-  'PASSWORD',
+  '<PASSWORD>',
   process.env.DATABASE_PASSWORD
 );
 
+// .connect(process.env.DATABASE_LOCAL, {
 mongoose
   .connect(DB, {
+    useUnifiedTopology: true,
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
   })
-  .then(() => console.log('DB connection successful!'));
+  .then(() => {
+    console.log('DB connection successful');
+  });
 
+//console.log(app.get('env'));
+//global vars that say what environment we are in
+
+//prints development
+
+//process.env.PORT is mandatory for heroku, it assigns a random port as a env. variable
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
@@ -31,15 +43,12 @@ const server = app.listen(port, () => {
 
 process.on('unhandledRejection', (err) => {
   console.log('UNHANDLED REJECTION! 💥 Shutting down...');
-  console.log(err.name, err.message);
+  console.log(err.name, err.message, err.stack);
+  //gracefully closes down the server
   server.close(() => {
     process.exit(1);
   });
 });
-
-process.on('SIGTERM', () => {
-  console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
-  server.close(() => {
-    console.log('💥 Process terminated!');
-  });
-});
+//THIS IS A NON OPERATINAL ERROR SO YOU'LL GET A 500  ERROR CODE ON POSTMAN (client)
+//console.log(x);
+//this is an example of an uncaught exception
