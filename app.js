de8e802d136072d005f92bc8a56c58094e28864d
Cassandra Kornhiser <cvkornhiser@gmail.com>
2020-12-11 09:39:53 -0800

    Stripe Webhook Tweaks

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const viewsRouter = require('./routes/viewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
 const AppError = require('./routes/utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
+const bookingController = require('./controllers/bookingController');
 
 const app = express();
 
@@ -121,11 +122,11 @@ const limiter = rateLimit({
 
 app.use('/api', limiter); //this middleware will only be applied to the api route
 
-// app.post(
-//   '/webhook-checkout',
-//   express.raw({ type: 'application/json' }),
-//   bookingController.webhookCheckout
-// );
+app.post(
+  '/webhook-checkout',
+  express.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout
+);
 
 //Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' })); //middleware allows you to get incoming data
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const viewsController = require('../controllers/viewsController');
 const authController = require('../controllers/authController');
-
+// const bookingController = require('../controllers/bookingController');
 // router.get('/', (req, res) => {
 //   res.status(200).render('base', {
 //     tour: 'The Forrest Hiker',
@@ -15,7 +15,12 @@ router.get('/', authController.isLoggedIn, viewsController.getOverview);
 router.get('/tour/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', viewsController.getLogin);
 router.get('/me', authController.protect, viewsController.getAccount);
-router.get('/my-tours', authController.protect, viewsController.getMyTours);
+router.get(
+  '/my-tours',
+  // bookingController.createBookingCheckout,
+  authController.protect,
+  viewsController.getMyTours
+);
 router.post(
   '/submit-user-data',
   authController.protect,
