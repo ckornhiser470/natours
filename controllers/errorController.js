3b9cb499c8faa37321550e3ac7d3d564d1aed0a1
Cassandra Kornhiser <cvkornhiser@gmail.com>
2020-12-10 17:52:34 -0800

    Improved stripe implementation

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -62,7 +62,7 @@ const sendErrorProd = (err, req, res) => {
       //Programming or other unknown error so we don't want to share what it is with client
     }
     // 1) Log Error
-    console.error('ERROR 🤯', err);
+    console.error('ERROR 🤯', err, err.stack);
     //2) Send generic message
     return res.status(500).json({
       status: 'error',
